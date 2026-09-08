034873c98cb6bc1d7e5ee96f25ce6b1fdb462c52
ZamoranoPatricio <zamoranopatriciome@gmail.com>
2026-09-08 14:04:30 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:5500', // Ajustá si tu Live Server usa otro puerto o ruta
+    baseUrl: 'http://127.0.0.1:5500',
     setupNodeEvents(on, config) { },
   },
 });
